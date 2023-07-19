963b91e95aa468fdbe9f140fe91bf1152a0242e6
danielsafari143 <danielsafari143@gmail.com>
2023-07-19 20:45:15 +0200

    FIX ERRORS

--- decode_morse.rb
+++ decode_morse.rb
@@ -1,31 +1,31 @@
-#decode_morse.rb
-#ruby "/Users/andrea/Desktop/Microverse/Module 4/morse-code/decode_morse.rb"
+# decode_morse.rb
+# ruby "/Users/andrea/Desktop/Microverse/Module 4/morse-code/decode_morse.rb"
 
 def decode_a_char(char)
-  decodemessage = {".-" => "A" , "-..." => "B" , "-.-." => "C" , "-.." =>  "D" ,"." => "E", 
-    "..-." => "F" ,"--." => "G","...." => "H" ,".." => "I", ".---" => "J","-.-" => "K",
-    ".-.." => "L","--" => "M", "-." => "N","---" => "O",".--." => "P","--.-" => "Q" ,
-    ".-." => "R", "..." => "S", "-" => "T" ,"..-" => "U","...-"  => "V" ,".--" => "W" , 
-    "-..-" => "X","-.--" => "Y", "--.." => "Z" }
-    return decodemessage[char]
+  decodemessage = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+                    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K',
+                    '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q',
+                    '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W',
+                    '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z' }
+  decodemessage[char]
 end
 
 def decode_word(word)
-    decoded_string = word.split(" ")
-    decoded_word = ""
-    decoded_string.each { |n|
-      decoded_word += decode_a_char(n)
-    }
-    return decoded_word   
+  decoded_string = word.split
+  decoded_word = ''
+  decoded_string.each do |n|
+    decoded_word += decode_a_char(n)
+  end
+  decoded_word
 end
 
-def decode_message(dec_word) 
-  decoded_string = dec_word.split("   ")
-  decoded_phrase = ""
-  decoded_string.each {|n| 
-    decoded_phrase += " #{decode_word(n)}" 
-  }
-  return decoded_phrase
+def decode_message(dec_word)
+  decoded_string = dec_word.split('   ')
+  decoded_phrase = ''
+  decoded_string.each do |n|
+    decoded_phrase += " #{decode_word(n)}"
+  end
+  decoded_phrase
 end
 
-puts decode_message (".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
