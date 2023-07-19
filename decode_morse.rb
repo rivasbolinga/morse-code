3fd49cbc1b7e8bcad53e6de0cbfdad3394d7abe9
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-19 15:35:50 +0200

    Create method to decode every char until letter j

--- decode_morse.rb
+++ decode_morse.rb
@@ -4,7 +4,36 @@ def decode_a_char(char)
   case char
     when '-.'
       return 'a'
+    when '-...'
+      return 'b'
+    when '-.-.'
+      return 'c'
+    when '-..'
+      return 'd'
+    when '.'
+      return 'e'
+    when '..-.'
+      return 'f'
+    when '--.'
+      return 'g'
+    when '....'
+      return 'h'
+    when '..'
+      return 'i'
+    when '.---'
+      return 'j'
+    else
+      return ''
   end
 end
 
+def decode_word(word)
+  decoded_string = ""
+  word.each_char { |c|
+ decoded_string += decode_a_char(c)
+}
+return decoded_string
+end
+
 puts decode_a_char('-.')
+puts decode_word('-...-.-...-.')
