148eb37cca7d5c9cb3e9e5f51e0c321a4ee9de0e
danielsafari143 <danielsafari143@gmail.com>
2023-07-19 19:43:01 +0200

    REFACTOR

--- decode_morse.rb
+++ decode_morse.rb
@@ -1,71 +1,23 @@
 #decode_morse.rb
 #ruby "/Users/andrea/Desktop/Microverse/Module 4/morse-code/decode_morse.rb"
+
 def decode_a_char(char)
-  case char
-    when '-.'
-      return 'a'
-    when '-...'
-      return 'b'
-    when '-.-.'
-      return 'c'
-    when '-..'
-      return 'd'
-    when '.'
-      return 'e'
-    when '..-.'
-      return 'f'
-    when '--.'
-      return 'g'
-    when '....'
-      return 'h'
-    when '..'
-      return 'i'
-    when '.---'
-      return 'j'
-    when '-.-'
-      return 'k'
-    when '.-..'
-      return 'l'
-    when '--'
-      return 'm'
-    when '-.'
-      return 'n'
-    when '----'
-      return 'o'
-    when '.--.'
-      return 'p'
-    when '--.-'
-      return 'q'
-    when '.-.'
-      return 'r'
-    when '...'
-      return 's'
-    when '-'
-      return 't'
-    when '..-'
-      return 'u'
-    when '...-'
-      return 'v'
-    when '.--'
-      return 'w'
-    when '-..-'
-      return 'x'
-    when '-.--'
-      return 'y'
-    when '--..'
-      return 'z'
-    else
-      return ''
-  end
+  decodemessage = {".-" => "A" , "-..." => "B" , "-.-." => "C" , "D" => "-.." ,"E" => "." , 
+    "F" => "..-." , "G" => "--." , "H" => "...." , "I" => ".." , "J" => ".---" , "K" => "-.-" ,
+    "L" => ".-.." , "M" => "--" , "N" => "-." , "O" => "---", "P" => ".--." ,  "Q" => "--.-" ,
+    "R" => ".-." , "S" => "...",  "T" => "-" , "U" => "..-" ,"V" => "...-" ,"W" => ".--" , 
+    "X" => "-..-", "Y" => "-.--","Z" => "--.. "}
+    
+    return decodemessage[char]
 end
 
-def decode_word(word)
-  decoded_string = ""
-  word.each_char { |c|
- decoded_string += decode_a_char(c)
-}
-return decoded_string
-end
+#def decode_word(word)
+ # decoded_string = ""
+  #word.each_char { |c|
+ #decoded_string += decode_a_char(c)
+#}
+#return decoded_string
+#end
 
-puts decode_a_char('-.')
-puts decode_word('-...-.-...-.')
+puts decode_a_char("-...")
+#puts decode_word("-- -.--   -. .- -- .")
