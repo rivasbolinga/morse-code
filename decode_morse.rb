cef553f0a6fec7a438f436d272414d988c9c71e8
danielsafari143 <danielsafari143@gmail.com>
2023-07-19 20:31:48 +0200

    ADD DECODE FUNCTION

--- decode_morse.rb
+++ decode_morse.rb
@@ -2,22 +2,30 @@
 #ruby "/Users/andrea/Desktop/Microverse/Module 4/morse-code/decode_morse.rb"
 
 def decode_a_char(char)
-  decodemessage = {".-" => "A" , "-..." => "B" , "-.-." => "C" , "D" => "-.." ,"E" => "." , 
-    "F" => "..-." , "G" => "--." , "H" => "...." , "I" => ".." , "J" => ".---" , "K" => "-.-" ,
-    "L" => ".-.." , "M" => "--" , "N" => "-." , "O" => "---", "P" => ".--." ,  "Q" => "--.-" ,
-    "R" => ".-." , "S" => "...",  "T" => "-" , "U" => "..-" ,"V" => "...-" ,"W" => ".--" , 
-    "X" => "-..-", "Y" => "-.--","Z" => "--.. "}
-    
+  decodemessage = {".-" => "A" , "-..." => "B" , "-.-." => "C" , "-.." =>  "D" ,"." => "E", 
+    "..-." => "F" ,"--." => "G","...." => "H" ,".." => "I", ".---" => "J","-.-" => "K",
+    ".-.." => "L","--" => "M", "-." => "N","---" => "O",".--." => "P","--.-" => "Q" ,
+    ".-." => "R", "..." => "S", "-" => "T" ,"..-" => "U","...-"  => "V" ,".--" => "W" , 
+    "-..-" => "X","-.--" => "Y", "--.." => "Z" }
     return decodemessage[char]
 end
 
-#def decode_word(word)
- # decoded_string = ""
-  #word.each_char { |c|
- #decoded_string += decode_a_char(c)
-#}
-#return decoded_string
-#end
+def decode_word(word)
+    decoded_string = word.split(" ")
+    decoded_word = ""
+    decoded_string.each { |n|
+      decoded_word += decode_a_char(n)
+    }
+    return decoded_word   
+end
 
-puts decode_a_char("-...")
-#puts decode_word("-- -.--   -. .- -- .")
+def decode_message(dec_word) 
+  decoded_string = dec_word.split("   ")
+  decoded_phrase = ""
+  decoded_string.each {|n| 
+    decoded_phrase += " #{decode_word(n)}" 
+  }
+  return decoded_phrase
+end
+
+puts decode_message (".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
